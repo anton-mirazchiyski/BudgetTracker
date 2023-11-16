ee27eee4b730722a63f8192f0ce8a4bc8095938f
Anton Mirazchiyski <anton.mirazchiyski@gmail.com>
2023-11-16 21:05:21 +0200

    Custom auth

--- budget_tracker/accounts/admin.py
+++ budget_tracker/accounts/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from budget_tracker.accounts.models import BudgetTrackerUser
 
-# Register your models here.
+
+class CustomUserAdmin(admin.ModelAdmin):
+    list_display = ['first_name', 'last_name', 'email']
+
+
+admin.site.register(BudgetTrackerUser, CustomUserAdmin)
--- budget_tracker/accounts/models.py
+++ budget_tracker/accounts/models.py
@@ -1,3 +1,65 @@
 from django.db import models
+from django.contrib.auth.models import BaseUserManager, AbstractBaseUser
 
-# Create your models here.
+
+class MyCustomUserManager(BaseUserManager):
+    def create_user(self, email, first_name, last_name, password=None):
+        if not email:
+            raise ValueError("Users must have an email address")
+
+        user = self.model(
+            email=self.normalize_email(email),
+            first_name=first_name,
+            last_name=last_name,
+        )
+
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, first_name, last_name, password=None):
+        user = self.create_user(
+            email,
+            password=password,
+            first_name=first_name,
+            last_name=last_name
+        )
+        user.is_admin = True
+        user.save(using=self._db)
+        return user
+
+
+class BudgetTrackerUser(AbstractBaseUser):
+    email = models.EmailField(max_length=255, unique=True, blank=False, null=False)
+
+    first_name = models.CharField(max_length=150, blank=False, null=False)
+
+    last_name = models.CharField(max_length=150, blank=False, null=False)
+
+    is_active = models.BooleanField(default=True)
+
+    is_admin = models.BooleanField(default=False)
+
+    objects = MyCustomUserManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['first_name', 'last_name']
+
+    def __str__(self):
+        return self.email
+
+    def get_full_name(self):
+        return f'{self.first_name} {self.last_name}'
+
+    def get_short_name(self):
+        return self.first_name
+
+    def has_perm(self, perm, obj=None):
+        return True
+
+    def has_module_perms(self, app_label):
+        return True
+
+    @property
+    def is_staff(self):
+        return self.is_admin
