482ce265835d957ad731cea2a7512196c85c0a3d
Anton Mirazchiyski <anton.mirazchiyski@gmail.com>
2023-11-16 15:39:44 +0200

    Create app accounts
    Authentication app

--- budget_tracker/urls.py
+++ budget_tracker/urls.py
@@ -3,8 +3,9 @@ URL configuration for budget_tracker project.
 """
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('users/', include('budget_tracker.accounts.urls')),
 ]
