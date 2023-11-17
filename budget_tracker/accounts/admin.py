ed68e862db8db5aebf8924c88a65cbd5a1f4be12
Anton Mirazchiyski <anton.mirazchiyski@gmail.com>
2023-11-17 18:53:49 +0200

    Add admin customization

--- budget_tracker/accounts/admin.py
+++ budget_tracker/accounts/admin.py
@@ -3,6 +3,9 @@ from budget_tracker.accounts.models import BudgetTrackerUser
 
 
 class CustomUserAdmin(admin.ModelAdmin):
+    fields = (['first_name', 'last_name', 'email', 'password'] +
+              ['last_login', 'is_active', 'is_admin'])
+
     list_display = ['first_name', 'last_name', 'email']
 
 
