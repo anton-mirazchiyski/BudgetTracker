ceb844ca99ad038a6d320e5ed9b70d1492111e47
Anton Mirazchiyski <anton.mirazchiyski@gmail.com>
2023-11-19 11:28:16 +0200

    Create app common

--- budget_tracker/settings.py
+++ budget_tracker/settings.py
@@ -31,6 +31,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    'budget_tracker.common',
     'budget_tracker.accounts',
 ]
 
--- budget_tracker/urls.py
+++ budget_tracker/urls.py
@@ -7,5 +7,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('budget_tracker.common.urls')),
     path('users/', include('budget_tracker.accounts.urls')),
 ]
