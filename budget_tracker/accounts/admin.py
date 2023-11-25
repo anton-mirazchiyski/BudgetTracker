7c6e8f1eab45eb99c22f462753cba409a14b806a
Anton Mirazchiyski <anton.mirazchiyski@gmail.com>
2023-11-25 12:18:46 +0200

    Customize admin
    For the new models related to User

--- budget_tracker/accounts/admin.py
+++ budget_tracker/accounts/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from budget_tracker.accounts.models import BudgetTrackerUser
+from budget_tracker.accounts.models import BudgetTrackerUser, UserProfile, Currency
 
 
 class CustomUserAdmin(admin.ModelAdmin):
@@ -10,3 +10,29 @@ class CustomUserAdmin(admin.ModelAdmin):
 
 
 admin.site.register(BudgetTrackerUser, CustomUserAdmin)
+
+
+class UserProfileAdmin(admin.ModelAdmin):
+    list_display = ['first_name', 'last_name', 'email']
+
+    @staticmethod
+    def first_name(obj):
+        return obj.user.first_name
+
+    @staticmethod
+    def last_name(obj):
+        return obj.user.last_name
+
+    @staticmethod
+    def email(obj):
+        return obj.user.email
+
+
+admin.site.register(UserProfile, UserProfileAdmin)
+
+
+class CurrencyAdmin(admin.ModelAdmin):
+    list_display = ['user_profile', 'currency']
+
+
+admin.site.register(Currency, CurrencyAdmin)
