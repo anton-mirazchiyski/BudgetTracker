d28f7605f4243d100e9429db9b9f5a2c0938b44c
Anton Mirazchiyski <anton.mirazchiyski@gmail.com>
2023-11-29 23:08:53 +0200

    Work on main income template

--- budget_tracker/income/urls.py
+++ budget_tracker/income/urls.py
@@ -1,1 +1,9 @@
-urlpatterns = []
+from django.urls import path
+
+from budget_tracker.income.views import IncomeListView
+
+app_name = 'income'
+
+urlpatterns = [
+    path('all/', IncomeListView.as_view(), name='all-income'),
+]
--- budget_tracker/income/views.py
+++ budget_tracker/income/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from django.views import generic as views
 
-# Create your views here.
+from budget_tracker.income.models import Income
+
+
+def get_user_pk(request):
+    user_pk = request.user.pk
+    return user_pk
+
+
+class IncomeListView(views.ListView):
+    model = Income
+    template_name = 'income/income-list-page.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['profile_pk'] = get_user_pk(self.request)
+        return context
