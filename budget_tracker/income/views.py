ec783aa5193af4e2417fb4308ec5140c509a6709
Anton Mirazchiyski <anton.mirazchiyski@gmail.com>
2023-11-30 11:52:23 +0200

    Override get_queryset method

--- budget_tracker/income/views.py
+++ budget_tracker/income/views.py
@@ -1,12 +1,19 @@
 from django.shortcuts import render
 from django.views import generic as views
 
+from budget_tracker.accounts.models import UserProfile
 from budget_tracker.income.models import Income
 
 
 def get_user_pk(request):
     user_pk = request.user.pk
     return user_pk
+
+
+def get_user_profile(request):
+    user = request.user
+    user_profile = UserProfile.objects.filter(user=user).get()
+    return user_profile
 
 
 class IncomeListView(views.ListView):
@@ -17,3 +24,8 @@ class IncomeListView(views.ListView):
         context = super().get_context_data(**kwargs)
         context['profile_pk'] = get_user_pk(self.request)
         return context
+
+    def get_queryset(self):
+        user_profile = get_user_profile(self.request)
+        self.queryset = user_profile.income_set.all()
+        return super().get_queryset()
