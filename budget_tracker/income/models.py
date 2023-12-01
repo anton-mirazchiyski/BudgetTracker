96858f8d9106ef488559fa47dfe7b61a6939cbd7
Anton Mirazchiyski <anton.mirazchiyski@gmail.com>
2023-12-01 21:49:40 +0200

    Income view and template

--- budget_tracker/income/models.py
+++ budget_tracker/income/models.py
@@ -1,13 +1,15 @@
 from django.core.validators import MinValueValidator
 from django.db import models
 
-from budget_tracker.accounts.models import UserProfile
+from budget_tracker.accounts.models import UserProfile, Currency
 
 
 class Income(models.Model):
     source = models.CharField(max_length=30, null=False, blank=False)
 
     amount = models.FloatField(validators=[MinValueValidator(0)], null=False, blank=False)
+
+    currency = models.CharField(max_length=30, null=True, blank=True)
 
     submitted_on = models.DateTimeField(auto_now_add=True)
 
--- budget_tracker/income/views.py
+++ budget_tracker/income/views.py
@@ -1,4 +1,6 @@
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
+from django.urls import reverse_lazy
 from django.views import generic as views
 
 from budget_tracker.core.accounts_utils import get_user_profile
@@ -26,3 +28,14 @@ class IncomeAddView(views.CreateView):
     model = Income
     template_name = 'income/income-add-page.html'
     form_class = IncomeForm
+    success_url = reverse_lazy('income:all-income')
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['current_currency'] = get_current_currency(self.request)
+        return context
+
+    def post(self, request, *args, **kwargs):
+        super().post(request, *args, **kwargs)
+        self.queryset.add(self.object)
+        return HttpResponseRedirect(super().get_success_url())
