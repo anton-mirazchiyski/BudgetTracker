e88586aa8239059d37f3ceeccf81273327416c54
Anton Mirazchiyski <anton.mirazchiyski@gmail.com>
2024-02-06 21:24:53 +0200

    Add Fontawesome to the project

--- budget_tracker/settings.py
+++ budget_tracker/settings.py
@@ -31,6 +31,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    'fontawesomefree',
     'budget_tracker.common',
     'budget_tracker.accounts',
     'budget_tracker.income',
