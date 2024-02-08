03979d723b282ed2da4162541f150f94be9ccbdb
Anton Mirazchiyski <anton.mirazchiyski@gmail.com>
2024-02-08 11:20:16 +0200

    Add new field to Income model

--- budget_tracker/income/models.py
+++ budget_tracker/income/models.py
@@ -5,9 +5,22 @@ from budget_tracker.accounts.models import UserProfile, Currency
 
 
 class Income(models.Model):
+
+    EARNED_INCOME = 'Earned Income'
+    PASSIVE_INCOME = 'Passive Income'
+    PORTFOLIO_INCOME = 'Portfolio Income'
+
+    INCOME_CHOICES = (
+        (EARNED_INCOME, EARNED_INCOME),
+        (PASSIVE_INCOME, PASSIVE_INCOME),
+        (PORTFOLIO_INCOME, PORTFOLIO_INCOME),
+    )
+
     source = models.CharField(max_length=30, null=False, blank=False)
 
     amount = models.FloatField(validators=[MinValueValidator(0)], null=False, blank=False)
+
+    type = models.CharField(max_length=30, choices=INCOME_CHOICES, null=False, blank=False)
 
     currency = models.CharField(max_length=30, null=True, blank=True)
 
