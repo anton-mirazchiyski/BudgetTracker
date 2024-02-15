77c585d9c83980b7be75a63469e9c60fe57e79c4
Anton Mirazchiyski <anton.mirazchiyski@gmail.com>
2024-02-15 20:51:27 +0200

    Style

--- budget_tracker/income/forms.py
+++ budget_tracker/income/forms.py
@@ -7,14 +7,14 @@ class IncomeAddForm(forms.ModelForm):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         for field in self.fields:
-            self.fields[field].widget.attrs['class'] = 'form-control'
+            self.fields[field].widget.attrs['class'] = 'income-form-input'
 
     class Meta:
         model = Income
         fields = ('source', 'amount', 'type')
 
         labels = {
-            'source': 'Source of Income',
-            'amount': 'Amount',
-            'type': 'Type of Income',
+            'source': 'Source of Income:',
+            'amount': 'Amount:',
+            'type': 'Type of Income:',
         }
