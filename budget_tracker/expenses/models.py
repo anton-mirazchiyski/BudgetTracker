767dea3cb5f04b1e870c7bb9f507dc7f4ed697b3
Anton Mirazchiyski <anton.mirazchiyski@gmail.com>
2024-02-25 21:32:20 +0200

    Create model Expense

--- budget_tracker/expenses/models.py
+++ budget_tracker/expenses/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+from budget_tracker.accounts.models import UserProfile
+
+
+class Expense(models.Model):
+    description = models.TextField(null=True, blank=True)
+
+    date = models.DateField(null=False, blank=False)
+
+    amount = models.DecimalField(max_digits=7, decimal_places=2, null=False, blank=False)
+
+    currency = models.CharField(max_length=30, null=False, blank=True)
+
+    profile = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
