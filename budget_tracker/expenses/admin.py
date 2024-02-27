cec6081498dc897141e2a02d96dfb585b150d807
Anton Mirazchiyski <anton.mirazchiyski@gmail.com>
2024-02-27 16:25:59 +0200

    Register 'Expense' to admin

--- budget_tracker/expenses/admin.py
+++ budget_tracker/expenses/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from budget_tracker.expenses.models import Expense
+
+
+class ExpenseAdmin(admin.ModelAdmin):
+    list_display = ('date', 'amount', 'currency', 'profile')
+
+
+admin.site.register(Expense, ExpenseAdmin)
