3b91ca494e410e613bec748d8de9d8b79e4e86fb
Anton Mirazchiyski <anton.mirazchiyski@gmail.com>
2024-02-29 22:24:53 +0200

    Add 'add expense' view and URL

--- budget_tracker/expenses/urls.py
+++ budget_tracker/expenses/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from budget_tracker.expenses.views import ExpenseListView
+from budget_tracker.expenses.views import ExpenseListView, add_expense
 
 app_name = 'expenses'
 
 urlpatterns = [
         path('', ExpenseListView.as_view(), name='all-expenses'),
+        path('add/', add_expense, name='add-expense'),
 ]
--- budget_tracker/expenses/views.py
+++ budget_tracker/expenses/views.py
@@ -1,8 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views import generic as views
 
 from budget_tracker.core.accounts_utils import get_user_profile
 from budget_tracker.core.currencies_utils import get_current_currency
+from budget_tracker.expenses.forms import ExpenseAddForm
 from budget_tracker.expenses.models import Expense
 
 
@@ -19,3 +20,18 @@ class ExpenseListView(views.ListView):
         user_profile = get_user_profile(self.request)
         self.queryset = user_profile.expense_set.all()
         return super().get_queryset()
+
+
+def add_expense(request):
+    if request.method == 'POST':
+        form = ExpenseAddForm(request.POST)
+        if form.is_valid():
+            expense = form.save(commit=False)
+            profile = get_user_profile(request)
+            expense.currency = get_current_currency(request)
+            expense.profile = profile
+            expense.save()
+            return redirect('expenses:all-expenses')
+    form = ExpenseAddForm()
+
+    return render(request, 'expenses/expense-add-page.html', {'form': form})
