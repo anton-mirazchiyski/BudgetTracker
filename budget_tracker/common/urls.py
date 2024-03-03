e025371f6c1990fcb99f730393754fe6cee6c8d9
Anton Mirazchiyski <anton.mirazchiyski@gmail.com>
2024-03-03 22:16:58 +0200

    Add feature for balance

--- budget_tracker/common/urls.py
+++ budget_tracker/common/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from budget_tracker.common.views import index
+from budget_tracker.common.views import index, show_balance
 
 app_name = 'common'
 
 urlpatterns = [
     path('', index, name='home'),
+    path('balance/', show_balance, name='balance'),
 ]
--- budget_tracker/common/views.py
+++ budget_tracker/common/views.py
@@ -2,6 +2,8 @@ from django.core.exceptions import ObjectDoesNotExist
 from django.shortcuts import render
 from django.views import generic as views
 
+from budget_tracker.core.accounts_utils import get_user_profile
+
 
 def index(request):
     try:
@@ -15,3 +17,24 @@ def index(request):
         'common/home-page.html',
         {'profile_pk': profile_pk}
     )
+
+
+def calculate_balance(profile):
+    all_income = profile.income_set.all()
+    sum_of_income = sum([income.amount for income in all_income])
+    all_expenses = profile.expense_set.all()
+    sum_of_expenses = sum([expense.amount for expense in all_expenses])
+
+    if sum_of_income - sum_of_expenses < 0:
+        current_balance = 0
+    else:
+        current_balance = sum_of_income - sum_of_expenses
+    return current_balance
+
+
+def show_balance(request):
+    profile = get_user_profile(request)
+    balance = calculate_balance(profile)
+    context = {'balance': balance}
+
+    return render(request,'common/balance-page.html', context)
