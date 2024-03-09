3cae1e63058f58c5853d728564f031f1c235edb6
Anton Mirazchiyski <anton.mirazchiyski@gmail.com>
2024-03-09 23:40:41 +0200

    Start dashboard page
    Attempt to implement a chart.

--- budget_tracker/common/urls.py
+++ budget_tracker/common/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from budget_tracker.common.views import IndexView, show_balance
+from budget_tracker.common.views import IndexView, show_balance, show_dashboard
 
 app_name = 'common'
 
 urlpatterns = [
     path('', IndexView.as_view(), name='home'),
+    path('dashboard/', show_dashboard, name='dashboard'),
     path('balance/', show_balance, name='balance'),
 ]
--- budget_tracker/common/views.py
+++ budget_tracker/common/views.py
@@ -1,13 +1,40 @@
-from django.core.exceptions import ObjectDoesNotExist
 from django.shortcuts import render
 from django.views import generic as views
 
 from budget_tracker.core.accounts_utils import get_user_profile
 from budget_tracker.core.common_utils import get_recent_transactions
+from budget_tracker.income.models import Income
 
 
 class IndexView(views.TemplateView):
     template_name = 'common/home-page.html'
+
+
+def month_mapper():
+    month_names = {
+        1: 'January',
+        2: 'February',
+        3: 'March',
+    }
+    return month_names
+
+
+def show_dashboard(request):
+    profile = get_user_profile(request)
+    all_income = Income.objects.filter(profile=profile).order_by('date')
+    months = []
+    amounts = []
+    amount_per_month = 0
+    for income in all_income:
+        current_month = income.date.month
+        if current_month not in months:
+            months.append(current_month)
+
+    months = [month_mapper()[month] for month in months]
+    labels = months
+    context = {'labels': labels}
+
+    return render(request, 'common/dashboard.html', context)
 
 
 def show_balance(request):
