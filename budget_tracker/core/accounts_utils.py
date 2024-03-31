cb2b14e3109b9d110dc868b2f06f30551bab39f4
Anton Mirazchiyski <anton.mirazchiyski@gmail.com>
2024-03-31 23:36:21 +0300

    Catch exception

--- budget_tracker/core/accounts_utils.py
+++ budget_tracker/core/accounts_utils.py
@@ -3,14 +3,12 @@ from django.core.exceptions import ObjectDoesNotExist
 from budget_tracker.accounts.models import UserProfile, ProfilePhoto
 
 
-def get_user_pk(request):
-    user_pk = request.user.pk
-    return user_pk
-
-
 def get_user_profile(request):
     user = request.user
-    user_profile = UserProfile.objects.filter(user=user).get()
+    try:
+        user_profile = UserProfile.objects.filter(user=user).get()
+    except TypeError:
+        user_profile = None
     return user_profile
 
 
