e3c184359411df7cf129663a837a02f6e765a54a
Anton Mirazchiyski <anton.mirazchiyski@gmail.com>
2024-11-01 11:15:23 +0200

    Update expense delete functionality

--- budget_tracker/core/expenses_utils.py
+++ budget_tracker/core/expenses_utils.py
@@ -1,12 +1,18 @@
-from budget_tracker.core.common_utils import add_to_balance, get_current_balance
+from budget_tracker.core.common_utils import add_to_balance, get_current_balance, get_recent_transactions
+from budget_tracker.income.models import Income
+
+
+def add_penultimate_transaction_to_balance_if_is_income(profile):
+    recent_transactions = get_recent_transactions(profile)[::-1]
+    penultimate_transaction = recent_transactions[-2]
+    if isinstance(penultimate_transaction, Income):
+        add_to_balance(profile, penultimate_transaction.amount)
 
 
 def determine_expense_return_amount_on_delete(profile, expense):
     current_balance = get_current_balance(profile)
     if current_balance.amount == 0:
-        last_income = profile.income_set.last()
-        if last_income:
-            add_to_balance(profile, last_income.amount)
+        add_penultimate_transaction_to_balance_if_is_income(profile)
     else:
         add_to_balance(profile, expense.amount)
 
